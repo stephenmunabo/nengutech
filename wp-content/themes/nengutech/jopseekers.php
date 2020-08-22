eaba3c8a1fa0f34fe6ea43f911ffa77125bc8388
Stephen Munabo <stephen.munabo@gmail.com>
2020-08-22 11:58:19 -0500

    initial commit

--- wp-content/themes/nengutech/jopseekers.php
+++ wp-content/themes/nengutech/jopseekers.php
@@ -459,7 +459,7 @@ get_header();
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        
         <h4 class="modal-title">AWS Solutions Architect</h4>
       </div>
       <div class="modal-body">
