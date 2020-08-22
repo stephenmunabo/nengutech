9189098cba15ed878a17123e692a10ee97f0b2f4
Stephen Munabo <stephen.munabo@gmail.com>
2020-08-22 11:53:59 -0500

    initial commit

--- wp-content/themes/nengutech/jopseekers.php
+++ wp-content/themes/nengutech/jopseekers.php
@@ -65,7 +65,7 @@ get_header();
                             <h4>Aws</h4>
                             <p>AWS Solution Architect course is aimed at empowering you with skills that will help you in the design of fault-tolerant, cost efficient & scalable distributed systems on platforms such as AWS.</p>
                         </div>
-                        <a href="" class="see-more-btn">Learn More <i class="far fa-arrow-right"></i></a>
+                        <a href="" data-toggle="modal" data-target="#modal-aws" class="see-more-btn">Learn More <i class="far fa-arrow-right"></i></a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
@@ -448,6 +448,32 @@ get_header();
             </div>
         </div>
     </div>
+    
+
+    <!--Modals-->
+
+    <!-- Modal -->
+<div id="modal-aws" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">AWS Solutions Architect</h4>
+      </div>
+      <div class="modal-body">
+        <p> AWS Solution Architect course is aimed at empowering you with skills that will help you in the design of fault-tolerant, cost efficient & scalable distributed systems on platforms such as AWS. More importantly, with skills leanred you will be proficient the crafting of advanced cloud-based solutions and migrating data to the cloud. 
+Based on the recommendations by Amazon, as an architect, you will be able to make plans for, design and scale AWS cloud implementations. Taking this course empowers you to be a AWS Solution architect, who can receive an average salary of $126,000 annually.  	</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
 
 
     <!-- call to action end -->
