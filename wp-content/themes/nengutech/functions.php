8831d9e7955a50724ff0ee1b7f386ef2e226760e
Stephen Munabo <stephen.munabo@gmail.com>
2020-08-21 21:28:52 -0500

    initial commit

--- wp-content/themes/nengutech/functions.php
+++ wp-content/themes/nengutech/functions.php
@@ -16,3 +16,12 @@ function register_my_menu() {
 }
 add_action( 'init', 'register_my_menu' );
 
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+  if (in_array('current-menu-item', $classes) ){
+    $classes[] = 'active-menu ';
+  }
+  return $classes;
+}
+
