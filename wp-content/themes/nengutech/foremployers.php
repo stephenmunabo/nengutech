98c356ddabdd75e7420fe9630b5ae870986020f8
Stephen Munabo <stephen.munabo@gmail.com>
2020-09-07 07:08:57 -0500

    upadtes

--- wp-content/themes/nengutech/foremployers.php
+++ wp-content/themes/nengutech/foremployers.php
@@ -222,7 +222,7 @@ get_header();
                             <h4>Register here and we will do the rest. </h4>
                         </div>
 
-                        <iframe width="600" height="850" src="http://office.nengu.tech/forms/wtl/69e213f77547e9e5316af3e3451b30ae" frameborder="0" allowfullscreen></iframe>
+                        <iframe width="600" height="850" src="https://office.nengu.tech/forms/wtl/69e213f77547e9e5316af3e3451b30ae" frameborder="0" allowfullscreen></iframe>
 
                         <form style="display: none" action="">
                             <div class="row">
