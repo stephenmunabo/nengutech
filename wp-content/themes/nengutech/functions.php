144a74cec2e8f70cdfa5d5b40ca5c5b3063dbe22
Stephen Munabo <stephen.munabo@gmail.com>
2020-09-08 17:13:14 -0500

    updated

--- wp-content/themes/nengutech/functions.php
+++ wp-content/themes/nengutech/functions.php
@@ -11,6 +11,17 @@
  
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'menus' );
+
+add_filter( 'gform_submit_button', 'add_custom_css_classes', 10, 2 );
+function add_custom_css_classes( $button, $form ) {
+    $dom = new DOMDocument();
+    $dom->loadHTML( '<?xml encoding="utf-8" ?>' . $button );
+    $input = $dom->getElementsByTagName( 'input' )->item(0);
+    $classes = $input->getAttribute( 'class' );
+    $classes .= " site-btn";
+    $input->setAttribute( 'class', $classes );
+    return $dom->saveHtml( $input );
+}
  
 function register_my_menu() {
   register_nav_menu('header-menu',__( 'Header Menu' ));
