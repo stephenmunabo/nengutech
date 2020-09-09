4bece1d41b522484cf3d7fe1e280e083438aac06
Stephen Munabo <stephen.munabo@gmail.com>
2020-09-09 07:26:11 -0500

    updated

--- wp-content/themes/nengutech/functions.php
+++ wp-content/themes/nengutech/functions.php
@@ -12,6 +12,17 @@
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'menus' );
 
+
+wp_enqueue_script("gforms_ui_datepicker", WP_PLUGIN_URL . "/gravityforms/js/jquery-ui/ui.datepicker.js", array("jquery"), "1.3.9", true);
+
+wp_enqueue_script("gforms_datepicker", WP_PLUGIN_URL . "/gravityforms/js/datepicker.js", array("gforms_ui_datepicker"), "1.3.9", true);
+
+wp_enqueue_script("gforms_conditional_logic_lib", WP_PLUGIN_URL . "/gravityforms/js/conditional_logic.js", array("gforms_ui_datepicker"), "1.3.9", true);
+
+wp_enqueue_style("gforms_css", WP_PLUGIN_URL . "/gravityforms/css/forms.css");
+
+
+
 add_filter( 'gform_submit_button', 'add_custom_css_classes', 10, 2 );
 function add_custom_css_classes( $button, $form ) {
     $dom = new DOMDocument();
