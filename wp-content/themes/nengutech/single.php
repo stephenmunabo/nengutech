cabbe87e482c325263f667542eaf30aeadce48c6
Stephen Munabo <stephen.munabo@gmail.com>
2020-09-09 11:49:19 -0500

    updated

--- wp-content/themes/nengutech/single.php
+++ wp-content/themes/nengutech/single.php
@@ -15,31 +15,21 @@ get_header();
 <div class="inner-blog-page">
         <div class="container">
             <div class="row">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="article-clean">
                     <div class="container">
                         <div class="row">
                             <div class="col-lg-10 col-xl-8 offset-lg-1 offset-xl-2">
                                 <div class="intro">
-                                    <h1 class="text-center">Your Wonderful Article Title</h1>
-                                    <p class="text-center"><span class="by">by</span> <a href="#">Author Name</a><span class="date">Sept 8th, 2016 </span></p><img class="img-fluid" src="assets/img/desk.jpg"></div>
-                                <div class="text">
-                                    <p>Sed lobortis mi. Suspendisse vel placerat ligula. <span style="text-decoration: underline;">Vivamus</span> ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in
-                                        justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-                                    <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac lacus. <strong>Ut vehicula rhoncus</strong> elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit <em>pulvinar dict</em> vel in justo. Vestibulum
-                                        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
-                                    <h2>Aliquam In Arcu </h2>
-                                    <p>Suspendisse vel placerat ligula. Vivamus ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-                                        posuere cubilia Curae.</p>
-                                    <figure><img class="figure-img" src="assets/img/beach.jpg">
-                                        <figcaption>Caption</figcaption>
-                                    </figure>
-                                    <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Suspendisse vel placerat ligula. Vivamus ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit
-                                        pulvinar dictum vel in justo.</p>
-                                </div>
+                                    <h1 class="text-center"><?php the_title(); ?></h1>
+                                    <p class="text-center"><span class="by">by</span> <a href="#">Author Name</a><span class="date"><?php echo get_the_date(); ?> </span></p><img class="img-fluid" src="assets/img/desk.jpg"></div>
+                                    <?php the_content(); ?>
                             </div>
                         </div>
                     </div>
                 </div>
+                <?php endwhile; ?>
+                <?php endif; ?>
             </div>
     </div>
 </div>
