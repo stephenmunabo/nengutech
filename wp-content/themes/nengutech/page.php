09f983eb9c6e27b632939074012ffc9bca767b23
Stephen Munabo <stephen.munabo@gmail.com>
2020-10-27 14:54:19 -0500

    added images

---
+++ wp-content/themes/nengutech/page.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+Template Name: Full-width layout
+Template Post Type: post, page, event
+*/
+
+get_header();
+
+
+?>
+
+
+<style>
+    .register-area {
+        background: #fff;
+        padding: 4px 0;
+    }
+</style>
+
+
+<?php the_content(); ?> 
+
+
+
+
+<?php
+
+get_footer();
