b59b031fc2a0ba4041621ed6abcfc7d371c177d0
Stephen Munabo <stephen.munabo@gmail.com>
2020-10-27 02:20:58 -0500

    added images

--- wp-content/themes/nengutech/functions.php
+++ wp-content/themes/nengutech/functions.php
@@ -121,23 +121,67 @@ function sendLeadToPerfex(){
 }
 //sendLeadToPerfex();;
 
-// Our custom post type function
-function create_posttype() {
+/*
+* Creating a function to create our CPT
+*/
  
-  register_post_type( 'programs',
-  // CPT Options
-      array(
-          'labels' => array(
-              'name' => __( 'Programs' ),
-              'singular_name' => __( 'Program' )
-          ),
-          'public' => true,
-          'has_archive' => true,
-          'rewrite' => array('slug' => 'Programs'),
+function custom_post_type() {
+ 
+  // Set UI labels for Custom Post Type
+      $labels = array(
+          'name'                => _x( 'Programs', 'Post Type General Name', 'twentytwenty' ),
+          'singular_name'       => _x( 'Program', 'Post Type Singular Name', 'twentytwenty' ),
+          'menu_name'           => __( 'Programs', 'twentytwenty' ),
+          'parent_item_colon'   => __( 'Parent Program', 'twentytwenty' ),
+          'all_items'           => __( 'All Programs', 'twentytwenty' ),
+          'view_item'           => __( 'View Program', 'twentytwenty' ),
+          'add_new_item'        => __( 'Add New Program', 'twentytwenty' ),
+          'add_new'             => __( 'Add New', 'twentytwenty' ),
+          'edit_item'           => __( 'Edit Program', 'twentytwenty' ),
+          'update_item'         => __( 'Update Program', 'twentytwenty' ),
+          'search_items'        => __( 'Search Program', 'twentytwenty' ),
+          'not_found'           => __( 'Not Found', 'twentytwenty' ),
+          'not_found_in_trash'  => __( 'Not found in Trash', 'twentytwenty' ),
+      );
+       
+  // Set other options for Custom Post Type
+       
+      $args = array(
+          'label'               => __( 'program', 'twentytwenty' ),
+          'description'         => __( 'Program news and reviews', 'twentytwenty' ),
+          'labels'              => $labels,
+          // Features this CPT supports in Post Editor
+          'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+          // You can associate this CPT with a taxonomy or custom taxonomy. 
+          'taxonomies'          => array( 'genres' ),
+          /* A hierarchical CPT is like Pages and can have
+          * Parent and child items. A non-hierarchical CPT
+          * is like Posts.
+          */ 
+          'hierarchical'        => false,
+          'public'              => true,
+          'show_ui'             => true,
+          'show_in_menu'        => true,
+          'show_in_nav_menus'   => true,
+          'show_in_admin_bar'   => true,
+          'menu_position'       => 5,
+          'can_export'          => true,
+          'has_archive'         => true,
+          'exclude_from_search' => false,
+          'publicly_queryable'  => true,
+          'capability_type'     => 'post',
           'show_in_rest' => true,
-
-      )
-  );
-}
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
+          'supports' => array( 'thumbnail' )
+      );
+       
+      // Registering your Custom Post Type
+      register_post_type( 'programs', $args );
+   
+  }
+   
+  /* Hook into the 'init' action so that the function
+  * Containing our post type registration is not 
+  * unnecessarily executed. 
+  */
+   
+  add_action( 'init', 'custom_post_type', 0 );
